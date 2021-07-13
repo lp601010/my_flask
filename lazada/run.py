ce8d030de379b786e7e0cad79f3deaaeaca6dc7c
lp601010 <15972080144@139.com>
2021-07-13 11:10:56 +0800

    加入时间列表

--- lazada/run.py
+++ lazada/run.py
@@ -86,52 +86,6 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
-
-@app.route('/25team', methods=["GET", "POST"])
-def team():
-    from pyecharts.charts import Line3D,Bar3D,Line
-    csv = pd.read_excel(r'C:\Users\Administrator\Downloads\25组（2018-2021年产品和销量）.xlsx')
-    df = csv.drop(['小组', 'ASIN', 'SKU', '2018全年度销售额', '2019全年度销售额', '2020全年度销售额', 'ASIN', '一级类目', '三级类目'], axis=1)
-    df['二级类目'] = df['二级类目'].fillna(value='其他')
-    df = df.groupby(['二级类目'], as_index=True).sum()
-    df.drop(df.columns[:15], axis=1, inplace=True)
-    df = df.astype('int')
-    line = (
-        Line()
-            .add_xaxis(df.columns.to_list()))
-    # .set_global_opts(splitline_opts=opts.SplitLineOpts(is_show=True))
-    for index, row in df.iterrows():
-        if row.sum()>50000:
-            # if index=="其他":
-            #     line.add_yaxis(index, row.tolist(), is_selected=True)
-            line.add_yaxis(index, row.tolist())
-
-    line.set_series_opts(
-                label_opts=opts.LabelOpts(is_show=False)
-            ).set_global_opts(
-        xaxis_opts=opts.AxisOpts(name="业绩年月",splitline_opts=opts.SplitLineOpts(is_show=True)),
-        yaxis_opts=opts.AxisOpts(name="销售额(美元)",splitline_opts=opts.SplitLineOpts(is_show=True)),
-        title_opts=opts.TitleOpts("25组销量曲线图")
-    )
-
-    # line=(
-    #     Line3D()
-    #         .add(
-    #         "",
-    #         data=df.values.tolist(),
-    #         xaxis3d_opts=opts.Axis3DOpts(data=df['时间'], type_="category"),
-    #         yaxis3d_opts=opts.Axis3DOpts(data=df['二级类目'], type_="category"),
-    #         zaxis3d_opts=opts.Axis3DOpts(type_="value")
-    #         # grid3d_opts=opts.Grid3DOpts(width=100, height=100, depth=100),
-    #     )
-    #     .set_global_opts(
-    #         xaxis_opts=opts.AxisOpts(name="业绩年月"),  # x轴名称
-    #         yaxis_opts=opts.AxisOpts(name="二级类目"),  # y轴名称
-    #         # zaxis_opts=opts.AxisOpts(name="销售额"),  # y轴名称
-    #     )
-    # )
-    return render_template('25team.html', myechart=line.render_embed())
-
 @app.route('/', methods=["GET", "POST"])
 @login_required
 def index():
@@ -142,6 +96,7 @@ def index():
     df = pd.DataFrame(columns=base.keys(), data=base)
     df['业绩年月'] = df['业绩年月'].map(lambda x: arrow.get(x).format('YYYY-MM'))
     df[['广告花费', '广告销售额']] = df[['广告花费', '广告销售额']].astype(int)
+
     df2 = df.groupby(['业绩年月'], as_index=False)["广告销售额", "广告花费"].sum()
     df2['ROI'] = df2["广告销售额"] / df2["广告花费"]
 
@@ -151,13 +106,15 @@ def index():
     df_tmp2=pd.DataFrame(data=df[df['广告类型'] == '联盟']['广告销售额'] / df[df['广告类型'] == '联盟']['广告花费'])
     df_tmp2.index = range(len(df_tmp2))
 
-
     df2['CPCROI'] = df_tmp
     df2['联盟ROI'] = df_tmp2
+
+    global time_list
+    time_list=df.groupby(['业绩年月'])['业绩年月'].first().tolist()
     bar = (
         Bar(init_opts=opts.InitOpts(theme=ThemeType.SHINE,
                                     animation_opts=opts.AnimationOpts(animation_easing="elasticOut")))
-            .add_xaxis(df.groupby(['业绩年月'])['业绩年月'].first().tolist())
+            .add_xaxis(time_list)
             .add_yaxis("CPC销售额", df[df['广告类型'] == 'CPC'].sort_values(by="业绩年月")["广告销售额"].tolist(), stack="stack1", z=0)
             .add_yaxis("联盟销售额", df[df['广告类型'] == '联盟'].sort_values(by="业绩年月")["广告销售额"].tolist(), stack="stack1", z=0)
             .set_series_opts(
@@ -208,9 +165,7 @@ def index():
     bar.overlap(line1)
     bar.overlap(line2)
     bar.overlap(line3)
-
-    form = Submit()
-    return render_template('index.html', myechart=bar.render_embed(),g=g,form=form)
+    return render_template('index.html', myechart=bar.render_embed(),g=g,time_list=[value for value in reversed(time_list)])
 
 
 
@@ -298,7 +253,8 @@ def show_table(ad_date=mn, host_name='', ad_type=''):
     df['业绩年月'] = df['业绩年月'].map(lambda x: arrow.get(x).format('YYYY-MM'))
     df = df.fillna(value='nan').applymap(lambda x: x.strip('nan') if '.' not in str(x) or '%' in str(x) else (
         str(x).split('.')[0] if abs(float(x)) > 100 and '%' not in str(x) else x))
-    return render_template('month_data.html', df=df, ad_type=ad_type, ad_date=ad_date)
+    # global time_list
+    return render_template('month_data.html', df=df, ad_type=ad_type, ad_date=ad_date,time_list=time_list)
 
 
 @app.route('/search', methods=["GET", "POST"])
